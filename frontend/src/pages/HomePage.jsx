7dcea52a1ca4c54cca3fcde984bf3d0a66165797
Diego Vilhlava <diegorocha450@yahoo.com>
2024-06-24 13:57:22 -0300

    added more components to the project

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
+import ProfileInfo from "../components/ProfileInfo"
+import Repos from "../components/Repos"
+import Search from "../components/Search"
+import SortRepos from "../components/SortRepos"
 
 
 const HomePage = () => {
   return (
-    <div>
-        <h1>Home Page</h1>
+    <div className="m-4">
+      <Search/>
+      <SortRepos/>
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-center">
+        <ProfileInfo/>
+        <Repos/>
+      </div>
     </div>
   )
 }
