96d7ace4b052441d6a5eb8531fb54c379e723174
Diego Vilhlava <diegorocha450@yahoo.com>
2024-06-27 11:56:11 -0300

    auth context finished

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,13 +4,16 @@ import passport from "passport"
 const router = express.Router()
 
 router.get('/github',passport.authenticate('github', { scope: [ 'user:email' ] }))
-router.get('/github/callback',passport.authenticate('github', { failureRedirect: process.env.CLIENT_BASE_URL+'/login' }),
-function(req, res) {
-  res.redirect(process.env.CLIENT_BASE_URL);
-})
+router.get(
+	"/github/callback",
+	passport.authenticate("github", { failureRedirect: process.env.CLIENT_BASE_URL + "/login" }),
+	function (req, res) {
+		res.redirect(process.env.CLIENT_BASE_URL);
+	}
+);
 
 router.get('/check',(req,res) => {
-    if (req.isAuthenticated) {
+    if (req.isAuthenticated()) {
         res.send({user:req.user})
     }else{
         res.send({user:null})
