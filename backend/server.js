40c1b2b3ba554426403f6c6e30efb59e2c046b31
Diego Vilhlava <diegorocha450@yahoo.com>
2024-06-28 10:50:10 -0300

    preparing for deploy

--- backend/server.js
+++ backend/server.js
@@ -8,25 +8,30 @@ import userRoutes from "./routes/user.route.js"
 import explorerRoutes from "./routes/explorer.route.js"
 import authRoutes from "./routes/auth.route.js"
 import connectMongoDB from "./db/conn.js"
-
+import path from "path"
 dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(cors())
 
-app.get("/",(req,res) => {
-    res.send('ok')
-})
+
 app.use('/api/auth',authRoutes)
 app.use('/api/users',userRoutes)
 app.use('/api/explore',explorerRoutes)
 
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
+})
 
 app.listen(5000,() => {
-    console.log('Server running')
+    console.log(`Server running on ${PORT}`)
     connectMongoDB()
 })
